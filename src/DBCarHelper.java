6a7b9e9a10edaf08a2ce26d4665fa6855c4858fe
Пламен Господинов <plamko3@abv.bg>
2021-04-24 17:30:50 +0300

    Added DBCarHelper and did some modifications to the MainPanel.

---
+++ src/DBCarHelper.java
@@ -0,0 +1,74 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class DBCarHelper {
+	
+	static Connection conn = null;
+	static PreparedStatement state = null;
+	static MyModel model = null;
+	static ResultSet result = null;
+	
+	//static ArrayList<String> firstCol = new ArrayList<String>();
+	
+	static MyModel getAllData() {
+		conn = getConnection();
+		String sql = "SELECT*FROM CARS";
+		try {
+			state = conn.prepareStatement(sql);
+			result = state.executeQuery();
+			model = new MyModel(result);
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return model;
+	}
+	
+	
+	//get Brands
+	/*static ArrayList<String> getBrandData() {
+		conn = getConnection();
+		String sql = "SELECT BRAND FROM BRANDS";
+		try {
+			state = conn.prepareStatement(sql);
+			result = state.executeQuery();
+			model = new MyModel(result);
+			firstCol = model.getBrands();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return firstCol;
+	}
+	*/
+	
+	static Connection getConnection() {
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/C:\\Users\\Plamen\\eclipse-workspace", "sa", "sa");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
